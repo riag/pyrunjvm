a0365d55f6c6642da1f30c7c74b128b4f2943e90
renjh <renjh@szca.com>
2021-08-09 14:20:19 +0800

    使用 importlib_metadata 来获取当前软件的版本

--- pyrunjvm/__init__.py
+++ pyrunjvm/__init__.py
@@ -1,4 +1,7 @@
-__version__ = '0.4.0'
+
+import importlib.metadata as importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
 
 from .application import create_application
 from .context import create_context
